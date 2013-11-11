24f7b57cd2ec2f971742eb6c1661b1fe47acde03
Vladimir Zdorovenco <vr.zdor@gmail.com>
2013-11-11 15:25:40 +0200

    remove blank lines

--- lib/x-editable-rails/view_helpers.rb
+++ lib/x-editable-rails/view_helpers.rb
@@ -16,19 +16,19 @@ module X
         #       - enumerable shorthand ['Yes', 'No', 'Maybe'] becomes { 'Yes' => 'Yes', 'No' => 'No', 'Maybe' => 'Maybe' }
         #     classes: a Hash of classes to add based on the value (same format and shorthands as source)
         #   value: override the object's value
-        #   
+        #
         def editable(object, method, options = {})
           options = Configuration.method_options_for(object, method).deep_merge(options).with_indifferent_access
           # merge data attributes for backwards-compatibility
           options.merge! options.delete(:data){ Hash.new }
-          
+
           url     = options.delete(:url){ polymorphic_path(object) }
           object  = object.last if object.kind_of?(Array)
           value   = options.delete(:value){ object.send(method) }
           source  = options[:source] ? format_source(options.delete(:source), value) : default_source_for(value)
           classes = format_source(options.delete(:classes), value)
           error   = options.delete(:e)
-          
+
           if xeditable? and can?(:edit, object)
             model   = object.class.name.split('::').last.underscore
             nid     = options.delete(:nid)
@@ -37,31 +37,31 @@ module X
               klass = nested ? object.class.const_get(nested.to_s.singularize.capitalize) : object.class
               klass.human_attribute_name(method)
             end
-            
+
             output_value = output_value_for(value)
             css_list = options.delete(:class).to_s.split(/s+/).unshift('editable')
             css_list << classes[output_value] if classes
-            
+
             css   = css_list.compact.uniq.join(' ')
             tag   = options.delete(:tag){ 'span' }
             placeholder = options.delete(:placeholder){ title }
-            
+
             # any remaining options become data attributes
             data  = {
-              type:   options.delete(:type){ default_type_for(value) }, 
-              model:  model, 
-              name:   method, 
-              value:  output_value, 
-              placeholder: placeholder, 
-              classes: classes, 
-              source: source, 
-              url:    url, 
-              nested: nested, 
+              type:   options.delete(:type){ default_type_for(value) },
+              model:  model,
+              name:   method,
+              value:  output_value,
+              placeholder: placeholder,
+              classes: classes,
+              source: source,
+              url:    url,
+              nested: nested,
               nid:    nid
             }.merge(options)
-            
+
             data.reject!{|_, value| value.nil?}
-            
+
             content_tag tag, class: css, title: title, data: data do
               source_values_for(value, source).join('<br/>').html_safe
             end
@@ -70,9 +70,9 @@ module X
             error || source_values_for(value, source).join('<br/>').html_safe
           end
         end
-        
+
         private
-        
+
         def output_value_for(value)
           value = case value
           when TrueClass
@@ -86,22 +86,22 @@ module X
           else
             value.to_s
           end
-          
+
           value.html_safe
         end
-        
+
         def source_values_for(value, source = nil)
           source ||= default_source_for value
-          
+
           values = Array.wrap(value)
-          
+
           if source
             values.map{|item| source[output_value_for item]}
           else
             values
           end
         end
-        
+
         def default_type_for(value)
           case value
           when TrueClass, FalseClass
@@ -112,14 +112,14 @@ module X
             'text'
           end
         end
-        
+
         def default_source_for(value)
           case value
           when TrueClass, FalseClass
             { '1' => 'Yes', '0' => 'No' }
           end
         end
-        
+
         # helper method that take some shorthand source definitions and reformats them
         def format_source(source, value)
           formatted_source = case value
@@ -132,10 +132,10 @@ module X
                 source.inject({}){|hash, key| hash.merge(key => key)}
               end
             end
-          
+
           formatted_source || source
         end
-        
+
       end
     end
   end
